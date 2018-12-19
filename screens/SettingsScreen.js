1cd764b58dc95d85c2044a32e901400123fafd14
Adrian Ross <adriantoddross@gmail.com>
2018-12-19 17:53:33 -0500

    Add state & set up textinput for testing countdown

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,48 +1,80 @@
 import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import {View, Text, TextInput, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+  // static navigationOptions = {
+  //   title: 'app.json',
+  // };
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: '00',
+      minutes: '0'
+    };
+    this.handleTimer = this.handleTimer.bind(this);
+  }
+
+  handleTimer(e) {
+    this.setState({
+      minutes: e
+    });
+  }
 
   render() {
     return (
-    <View style={styles.container}>
-      <View style={styles.layout}>
-        <Timer/>
-        <StartButton/>
+      <View style={styles.container}>
+        <View style={styles.layout}>
+          <TimerInput userInput={this.state.minutes} handleTimer={this.handleTimer}/>
+          <Timer minutes={this.state.minutes} seconds={this.state.seconds}/>
+          <StartButton/>
+        </View>
       </View>
-    </View>
     );
     
   }
 }
 
-  class Timer extends React.Component {
-    render() {
-      return(
-        <View>
-          <Text>
-            Clock time goes here.
-          </Text>
-        </View>
-      );
-    }
+class Timer extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>
+          {this.props.minutes}:{this.props.seconds}
+        </Text>
+      </View>
+    );
   }
+}
 
-  class StartButton extends React.Component {
-    render() {
-      return(
-        <View>
-          <Button
-            title="Start"
-          />
-        </View>
-      );
-    }
+class TimerInput extends React.Component {
+  render() {
+    return(
+      <View>
+        <TextInput
+          style={styles.textInput}
+          keyboardType='numeric'
+          onChangeText = {(e) => this.props.handleTimer(e)}
+          value={this.props.userInput}
+          maxLength={9}  //setting limit of input
+        />
+      </View>
+    );
   }
+}
+
+class StartButton extends React.Component {
+  render() {
+    return(
+      <View>
+        <Button
+          title="Start"
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
