64e49e4c39cf308f58aa0bf83eb6ab64bffc00f2
Adrian Ross <adriantoddross@gmail.com>
2018-12-19 16:25:13 -0500

    Stub timer & startbutton component

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -7,8 +8,51 @@ export default class SettingsScreen extends React.Component {
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+    <View style={styles.container}>
+      <View style={styles.layout}>
+        <Timer/>
+        <StartButton/>
+      </View>
+    </View>
+    );
+    
   }
 }
+
+  class Timer extends React.Component {
+    render() {
+      return(
+        <View>
+          <Text>
+            Clock time goes here.
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  class StartButton extends React.Component {
+    render() {
+      return(
+        <View>
+          <Button
+            title="Start"
+          />
+        </View>
+      );
+    }
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  layout: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+});
